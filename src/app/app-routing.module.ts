333dba30a5b9eb57a8000cacfce3abd390c94b09
Alvin-Karanja <alvin2@protonmail.com>
2020-01-15 07:51:34 +0300

    Imported login, lock components and defined routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {UserComponent} from './user/user.component';
-import {LoginComponent} from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './dashboard/home/home.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+import { TableComponent } from './dashboard/table/table.component';
+import { NotificationComponent } from './dashboard/notification/notification.component';
+
+import { RootComponent } from './dashboard/root/root.component';
+import { LoginComponent } from './page/login/login.component';
+import { LockComponent } from './page/lock/lock.component';
 
 const routes: Routes = [
-  { path: 'user', component: UserComponent },
-  { path: 'login', component: LoginComponent },
-  {path : '', component : LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'lock', component: LockComponent},
+  {path: 'dashboard', component: RootComponent, children: [
+    {path: '', component: HomeComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'table', component: TableComponent},
+    {path: 'notification', component: NotificationComponent}
+  ]}
 ];
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  declarations: []
-})
-export class AppRoutingModule { }
+
+export const routing = RouterModule.forRoot(routes);
+
