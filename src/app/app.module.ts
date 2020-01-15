4f8e10243d749be0742263ba504d546067fe5574
Alvin-Karanja <alvin2@protonmail.com>
2020-01-15 07:52:28 +0300

    Imported components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,72 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './dashboard/home/home.component';
+import 'hammerjs';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FigurecardComponent } from './shared/figurecard/figurecard.component';
+import { ImagecardComponent } from './shared/imagecard/imagecard.component';
+import { NotificationComponent } from './dashboard/notification/notification.component';
+import { MsgIconBtnComponent } from './shared/msgiconbtn/msgiconbtn.component';
+import { SweetalertComponent } from './dashboard/sweetalert/sweetalert.component';
+import { RootComponent } from './dashboard/root/root.component';
+import { LockComponent } from './page/lock/lock.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SettingsComponent } from './dashboard/setting/setting.component';
+import { PanelsComponent } from './component/panels/panels.component';
+import { SettingsService } from './services/settings.services';
+import { WizardComponent } from './component/wizard/wizard.component';
+import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { CustomMaterialModule } from './material.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './page/login/login.component';
 import { UserComponent } from './user/user.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+import { routing } from './app.routes';
+import { MatButtonModule, MatRadioModule, MatInputModule, MatMenuModule, MatCheckboxModule } from '@angular/material';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    SidebarComponent,
+    HomeComponent,
+    ProfileComponent,
+    NavbarComponent,
+    FigurecardComponent,
+    ImagecardComponent,
+    NotificationComponent,
+    MsgIconBtnComponent,
+    SweetalertComponent,
+    RootComponent,
+    LockComponent,
+    HeaderComponent,
+    FooterComponent,
+    SettingsComponent,
+    PanelsComponent,
+    WizardComponent,
     UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     CustomMaterialModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    routing,
+    MatButtonModule,
+    MatRadioModule,
+    MatInputModule,
+    MatMenuModule,
+    MatCheckboxModule
   ],
-  providers: [],
+  providers: [SettingsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
