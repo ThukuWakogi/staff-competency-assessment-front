cfeb913c81338d130c70eec8d26d8189ccfc7428
Alvin-Karanja <alvin2@protonmail.com>
2020-01-16 11:59:34 +0300

    Commited out deleted components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './dashboard/home/home.component';
 import { ProfileComponent } from './dashboard/profile/profile.component';
 import { NotificationComponent } from './dashboard/notification/notification.component';
-import { SweetalertComponent } from './dashboard/sweetalert/sweetalert.component';
-import { SettingsComponent } from './dashboard/setting/setting.component';
+// import { SweetalertComponent } from './dashboard/sweetalert/sweetalert.component';
+// import { SettingsComponent } from './dashboard/setting/setting.component';
 
 import { RootComponent } from './dashboard/root/root.component';
 import { LoginComponent } from './page/login/login.component';
-import { LockComponent } from './page/lock/lock.component';
+// import { LockComponent } from './page/lock/lock.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'lock', component: LockComponent},
+  // {path: 'lock', component: LockComponent},
   {path: 'dashboard', component: RootComponent, children: [
     {path: '', component: HomeComponent},
     {path: 'profile', component: ProfileComponent},
-    {path: 'notification', component: NotificationComponent},
-    {path: 'alert', component: SweetalertComponent},
-    {path: 'settings', component: SettingsComponent}
+    {path: 'notification', component: NotificationComponent}
+    // {path: 'alert', component: SweetalertComponent},
+    // {path: 'settings', component: SettingsComponent}
   ]}
 ];
 
