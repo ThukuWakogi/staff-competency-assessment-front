0f6eb7d7911fafc109eb861737b96b07dd2a6c89
Alvin-Karanja <alvin2@protonmail.com>
2020-01-16 12:00:08 +0300

    Defined route from login to dashboard

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {MatDialog} from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+firstname: string;
+email: string;
+password: string;
 
   ngOnInit() {
   }
+
+  login() : void {
+    if(this.firstname == 'admin' && this.password == 'admin'){
+     this.router.navigate(["user"]);
+    }else {
+      alert("Invalid credentials");
+    }
+  }
+  
 
 }
