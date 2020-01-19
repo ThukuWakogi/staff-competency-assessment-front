7f571cfc69882f853c589ff3a1a5a41232a60430
Alvin-Karanja <alvin2@protonmail.com>
2020-01-19 21:08:26 +0300

    Created root component

---
+++ src/app/HR Dashboard/root/root.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SettingsService } from '../../services/settings.services';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './root.component.html',
+  styleUrls: ['./root.component.scss']
+})
+export class RootComponent implements OnInit, OnDestroy {
+  public id: number;
+  public backgroundColor: string;
+  constructor(public settingService: SettingsService) {
+    this.id = settingService.getSidebarImageIndex() + 1;
+    this.backgroundColor = settingService.getSidebarColor();
+  }
+
+  ngOnInit() {
+    this.settingService.sidebarImageIndexUpdate.subscribe((id: number) => {
+      this.id = id + 1;
+    });
+    this.settingService.sidebarColorUpdate.subscribe((color: string) => {
+      this.backgroundColor = color;
+    });
+  }
+
+  ngOnDestroy() {
+    this.settingService.sidebarImageIndexUpdate.unsubscribe();
+    this.settingService.sidebarColorUpdate.unsubscribe();
+  }
+}
